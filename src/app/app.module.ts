74cb23dfc84580b086bdd5e295f9a5dcefd00961
Ruddy Campos <ruddyloq@gmail.com>
2018-11-03 08:57:23 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
+ 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// firebase 
+import {AngularFireModule} from 'angularfire2';
+import{AngularFireDatabaseModule} from 'angularfire2/database';
+import {environment} from '../environments/environment';
+
+// components
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component';
+
+//services
+
+import {ProductService} from './services/product.service'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
